33d2879d033bfee47f5b53e25df0e40a6c211048
mohammad-rakib-hasan <bdprakib9@gmail.com>
2024-10-21 17:41:36 +0600

    Js-loop

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+for (let i=1; i<=31; i++){
+    
+    console.log ("i am sorry for that you");
+    console.log (i)
+}
+
+let marks =[ 65, 48, 89, 29, 55, 22, 11, 91];
+
+for (let i=0; i<=7; i++){
+     
+    console.log(marks[i]);
+}
+
+let number = [65, 48, 89, 29, 55, 22, 11, 91, 70, 30, 10];
+
+                        for (let i=0; i<=10; i++){
+
+if(number[i] % 2 ==0){
+    console.log("This is an even number=" + number[i]);
+}
+
+else{
+    console.log("This is an odd number=" + number[i]);
+}
+}
+
